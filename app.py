0fcbf413c7d0cde27eac9d9c735309120a1af468
S <sarvesh.s@nokia.com>
2022-09-16 14:53:04 +0530

    modified in login

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return "hello-world"
+    return "hello-sarvesh and barath"
 
 if __name__ == "__main__":
     app.run(debug=True)
